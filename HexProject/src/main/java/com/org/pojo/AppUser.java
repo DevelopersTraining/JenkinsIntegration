22175f7ad439efa9406607985df3c3c3df469c8b
LTAT-TRN3\huser <hidancore@gmail.com>
2017-01-04 15:16:17 -0500

    First commit: Addes Entities @Dheeraj @Daniel

---
+++ HexProject/src/main/java/com/org/pojo/AppUser.java
@@ -0,0 +1,80 @@
+package com.org.pojo;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="APP_USER")
+public class AppUser {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="USER_ID")
+	private Long userId;
+	
+	@Column(name="ROLE_ID")
+	private Role roleId;
+	
+	@Column(name="USER_NAME")
+	private String userName;
+	
+	@Column(name="PASSWORD")
+	private String password;
+	
+	@Column(name="REG_DATE")
+	private Date regDate;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public Role getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(Role roleId) {
+		this.roleId = roleId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	
+	
+	
+	
+	
+	
+
+}
